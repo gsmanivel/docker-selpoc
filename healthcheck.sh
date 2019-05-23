285c953f090d2e46655759a00a48e56f0d7a121c
Subramaniyan <n0359906@lm.lmig.com>
2019-05-23 05:31:57 -0400

    healthcheck

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,3 +1,9 @@
+#!/usr/bin/env bash
+# Environment Variables
+# HUB_HOST
+# BROWSER
+# MODULE
+
 echo "Checking if hub is ready - $HUB_HOST"
 
 while [ "$( curl -s http://$HUB_HOST:4444/wd/hub/status | jq -r .value.ready )" != "true" ]
