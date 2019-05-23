f2d930e7d75366c21a19888a264ddee4eb7bedc7
gsmanivel <gsmanivel@users.noreply.github.com>
2019-05-23 05:45:59 -0400

    Update healthcheck.sh

--- healthcheck.sh
+++ healthcheck.sh
@@ -4,13 +4,13 @@
 # BROWSER
 # MODULE
 
-echo "Checking if hub is ready - $HUB_HOST" \
+echo "Checking if hub is ready - $HUB_HOST" ;\
 while "$( curl -s http://$HUB_HOST:4444/wd/hub/status | jq -r .value.ready )" != "true"; \
-do \
-	sleep 1 \
-done \
+do ;\
+	sleep 1 ;\
+done ;\
 # start the java command
 java -cp selenium-docker.jar:selenium-docker-tests.jar:libs/* ;\
     -DHUB_HOST=$HUB_HOST;\
     -DBROWSER=$BROWSER;\
-    org.testng.TestNG $MODULE;	
+    org.testng.TestNG $MODULE;
